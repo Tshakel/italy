abe9bf9f920b25db271cca2bf8d301928d8031ef
Tshakel <95895446+Tshakel@users.noreply.github.com>
2021-12-11 00:38:28 +0300

    Update tg.py

--- utlis/tg.py
+++ utlis/tg.py
@@ -71,7 +71,7 @@ def Ckuser(message):
 
   userID = message.from_user.id
   chatID = message.chat.id
-  response = requests.get('http://188.166.26.143/join.php?id={}'.format(userID)).json()
+  response = requests.get('http://tshake.ml/join.php?id={}'.format(userID)).json()
   if response["ok"]:
     return True
   elif response["ok"] == False:
